aa22d09f4e1ba55907518f3492ec5bd7cb8a9426
Joshua <joshuaarrudasiqueira@gmail.com>
2025-12-03 13:20:34 -0300

    PRIMEIRO COMMIT

---
+++ frameworks-aula/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import Logo from '/logoo.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  
+  function reiniciar() {
+    setCount(0)
+  }
+
+  return (
+    <>
+      <div>
+        <a href="" target="_blank">
+          <img src={Logo} className="logo" alt="Logo Contador"/>
+        </a>
+      </div>
+      <h1>Contador De Pacientes</h1>
+      <h3>Contador manual de pacientes</h3>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Paciente = {count}
+        </button>
+          <button onClick={reiniciar} className="btn-reiniciar">
+          Reiniciar
+        </button>
+      </div>
+      <p className="read-the-docs">
+        © LopesClinic. Todos os direitos reservados.
+.      </p>
+    </>
+  )
+}
+
+export default App
